396b932e003e47576f394c48ad0652a9d699c881
Ashvini Thomas <ashvinithomas@gmail.com>
2020-12-05 13:28:37 -0500

    created connection in connection.js

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: employeeTracker_db
+});
+
+connection.connect();
+
+//Setting up connection.query to use promises instead of callbacks
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
+
