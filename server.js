e5440ea8b62ae23f49ca44cebd6e533c75d2bbe4
Ashvini Thomas <ashvinithomas@gmail.com>
2020-12-05 18:39:34 -0500

    created function to update employee role and created two properties to be used to show employee choices

--- server.js
+++ server.js
@@ -154,3 +154,46 @@ async function viewEmployeesByDepartment() {
         
 }
 
+async function updateEmployeeRole() {
+    const employees = await db.findAllEmployees();
+
+    const employeeChoices = employees.map(({ id, first_name, last_name}) => ({
+        //create two properties: name and value for this object
+        name: first_name + " " + last_name,
+        value: id
+    }));
+
+    const { employeeId } = await prompt([
+        {
+            type: "list",
+            name: "employeeId",
+            message: "Which employee's role do you want to update?",
+            choices: employeeChoices
+        }
+    ]);
+
+    const roles = await db.findAllRoles();
+
+    const roleChoices = roles.map(({ id, title }) => ({
+        name: title,
+        value: id
+    }));
+
+    const { roleId } = await prompt ([
+        {
+            type: "list",
+            name: "roleId",
+            message: "Which role do you want to assign the selected employee?",
+            choices: roleChoices
+        }
+    ]);
+
+    await db.updateEmployeeRole(employeeId, roleId);
+
+    console.log("Update employee's role");
+    
+    loadMainPrompts();
+}
+
+
+
