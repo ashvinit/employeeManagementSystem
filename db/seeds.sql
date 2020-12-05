b871e4bcdddf60ef8c83d613b220ff98b0e2e719
Ashvini Thomas <ashvinithomas@gmail.com>
2020-12-05 13:22:47 -0500

    created config file and added connection.js file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,46 +1,41 @@
---DEPARTMENT--
-INSERT INTO department (name) VALUES ('Sales');
+use employeeTracker_db;
 
-INSERT INTO department (name) VALUES ('Engineering');
 
-INSERT INTO department (name) VALUES ('Finance');
-
-INSERT INTO department (name) VALUES ('Legal');
+-- DEPARTMENT --
+INSERT INTO department 
+    (name)
+VALUES 
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
 
 --ROLE--
-INSERT INTO role (title, salary, department_id) VALUES ('Customer Service Rep', 26000, 1);
-
-INSERT INTO role (title, salary, department_id) VALUES ('Customer Service Manager', 95000, 1);
-
-INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 93000, 2);
-
-INSERT INTO role (title, salary, department_id) VALUES ('Lead Engineer', 140000, 2);
-
-INSERT INTO role (title, salary, department_id) VALUES ('Accountant', 125000, 3);
-
-INSERT INTO role (title, salary, department_id) VALUES ('Bookkeeper', 45000, 3);
-
-INSERT INTO role (title, salary, department_id) VALUES ('Paralegal', 56000, 4);
-
-INSERT INTO role (title, salary, department_id) VALUES ('Lawyer', 120000, 4);
+INSERT INTO role 
+    (title, salary, department_id) 
+VALUES
+    ('Customer Service Rep', 26000, 1),
+    ('Customer Service Manager', 95000, 1),
+    ('Software Engineer', 93000, 2),
+    ('Lead Engineer', 140000, 2),
+    ('Accountant', 125000, 3),
+    ('Bookkeeper', 45000, 3),
+    ('Paralegal', 56000, 4),
+    ('Lawyer', 120000, 4);
 
 
 --EMPLOYEE--
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Michael', 'Scott', 2);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Dwight', 'Schrute', 1, 1);
-
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Pam', 'Beasley', 3);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jim', 'Halpert', 4, 3);
-
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Angela', 'Martin', 5);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Andy', 'Bernard', 6, 5);
-
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Ryan', 'Howard', 8);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Kelly', 'Kapoor', 7, 7);
+INSERT INTO employee 
+    (first_name, last_name, role_id, manager_id) 
+VALUES 
+    ('Michael', 'Scott', 2, NULL),
+    ('Dwight', 'Schrute', 1, 1),
+    ('Pam', 'Beasley', 3, NULL),
+    ('Jim', 'Halpert', 4, 3),
+    ('Angela', 'Martin', 5, NULL),
+    ('Andy', 'Bernard', 6, 5),
+    ('Ryan', 'Howard', 8, NULL),
+    ('Kelly', 'Kapoor', 7, 7);
 
 
