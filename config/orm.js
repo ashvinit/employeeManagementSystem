84c705d6bf1a97b1c9e8e68ab9213721584027a8
Ashvini Thomas <ashvinithomas@gmail.com>
2020-12-05 14:01:06 -0500

    added code to orm.js

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+//Import MySQL connection
+const connection = require("./connection.js");
+
+class DB {
+    //Keeping a refernce to the connection on the class in case we need it later
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+    //Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
+    findAllEmployees() {
+        return this.connection.query();
+    }
+}
